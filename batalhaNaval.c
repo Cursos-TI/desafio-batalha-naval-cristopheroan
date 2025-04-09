4233c3c36fe9c12342104207e7be6f6b256cff49
Cristopher Andrade <133789100+cristopheroan@users.noreply.github.com>
2025-04-09 22:08:14 +0000

    Implementando um tabuleiro e dois navios com vetor e matriz

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,7 +9,56 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int linhas[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    //Inicializando a matriz do tabuleiro em 0 representando a água.
+    int tabuleiro [10] [10] = {0};
+    
+        //Vetor com a posição do navio na horizontal (colunas 0,1,2)
+        int navioHorizontal[3] = {0,1,2};
 
+        //Vetor com a posição do navio na vertical (linhas 0,1,2)
+        int navioVertical[3] = {0,1,2};
+        
+        //declarando a posição do navio 1
+        for (int i = 0; i < 3; i++)
+        {
+            tabuleiro[5][navioHorizontal[i]] = 3; //O número 2 representa a linha que vai aparecer o 3 representa aparte do navio.
+        }
+
+        //declarando o navio 2
+        for (int i = 0; i < 3; i++)
+        {
+            //Verifica se o espaço esta vazio para evitar sobreposição
+            if (tabuleiro[navioVertical[i]][5] == 0)
+            {
+                tabuleiro[navioVertical[i]][5] = 3;//A ordem invertida indica que esta na vertical, 5 é a linha, 3 representa parte do navio
+            }
+            
+        }
+        
+        
+
+
+    //Representando o tabuleiro no console usando um loop aninhado.
+
+    printf("   ");
+    for (int j = 0; j < 10; j++)
+    {
+        printf("%c ", colunas[j]);
+    }
+      printf("\n ");
+    
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", linhas[i]);
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n ");//Nova linha ao final de cada linha.
+    }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
